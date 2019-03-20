866986b07ac5395d81e0f2f5da643b52090dc9d1
selenax <selenawenxu@gmail.com>
2019-03-19 23:25:31 -0700

    initialized mongo and mongoose with aws

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./services/passport.js');
+const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql');
-
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolver');
+const keys = require('./config/keys');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 //authRoutes returns a function
