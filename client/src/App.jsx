e81913bcd4d0ff076d8d0821a19f698b451a3441
selenax <selenawenxu@gmail.com>
2019-03-23 17:09:44 -0700

    added homepage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import HomePage from './HomePage.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a href="/auth/google">Sign in with Google</a>
-        </header>
+        <a href="/auth/google">Sign in with Google</a>
+        <HomePage />
       </div>
     );
   }
