f869e3ea2b657b1fdcfe8dd98fe4036b17c387d8
selenax <selenawenxu@gmail.com>
2019-03-23 18:18:05 -0700

    added google auth/user login state to redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App.jsx';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import * as serviceWorker from './serviceWorker'
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
