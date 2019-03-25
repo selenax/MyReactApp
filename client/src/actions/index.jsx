a8a218f1e3aa7a28efa476a7fe3b0585c2c610a7
selenax <selenawenxu@gmail.com>
2019-03-24 23:50:47 -0700

    added actionCreator for google auth for dispatch with reduxThunk

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -1,6 +1,25 @@
 import axios from 'axios';
 import { FETCH_USER } from './types.jsx';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+// redux without reduxThunk
+// const fetchUser = () => {
+//   const request = axios.get('/api/current_user');
+//   return {
+//     type: FETCH_USER,
+//     payload: request
+//   };
+// };
+
+// refactor with reduxThunk
+// export const fetchUser = () => {
+//   function(dispatch) {
+//     axios
+//       .get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+
+//refactor with es7
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res });
 };
