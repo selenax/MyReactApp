eede8fb47f911c4ee80d69b3047d6bd598e04e8c
selenax <selenawenxu@gmail.com>
2019-03-24 18:26:37 -0700

    added react router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-
-import HomePage from './HomePage.jsx';
-
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import Landing from './Landing.jsx';
+import NavBar from './NavBar.jsx';
+// class Landing extends Component {
+//   render() {
+//     return (
+//       <h2>
+//         <NavLink to="/auth/google" activeClassName="active">
+//           <span className="title">Search</span>
+//           <span className="fa fa-star" />
+//         </NavLink>
+//       </h2>
+//     );
+//   }
+// }
+const Header = () => <h2>Header</h2>;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <a href="/auth/google">Sign in with Google</a>
-        <HomePage />
+        <BrowserRouter>
+          <div>
+            <NavBar />
+            <Route exact path="/landing" component={Landing} />
+          </div>
+        </BrowserRouter>
+
+        <div />
       </div>
     );
   }
 }
 
-export default App;
+const AppWithRouter = withRouter(App);
+export default AppWithRouter;
