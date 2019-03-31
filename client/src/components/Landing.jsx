145ad704082dcc5110c66d69de2bbfb012784030
selenax <selenawenxu@gmail.com>
2019-03-30 23:10:12 -0700

    added landing page component, redirect user after  authentication

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> WHATSUP </h3>;
+      </div>
+    );
+  }
+}
+
+export default Landing;
