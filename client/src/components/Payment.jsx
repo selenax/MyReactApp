68ca3f6043b51787535f32b99dced326c28bfd52
selenax <selenawenxu@gmail.com>
2019-04-05 23:22:08 -0700

    added stripe to redux, payment successfully made with dummy credit card

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
+import * as actions from '../actions';
 
+//default stripe test cc# for testing 4242 4242 4242 4242
 class Payment extends Component {
   render() {
     return (
       <StripeCheckout
+        name=""
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleStripeToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       />
     );
   }
 }
 
-export default Payment;
+export default connect(
+  null,
+  actions
+)(Payment);
